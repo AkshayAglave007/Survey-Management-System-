13c0fe3fb765f1119523a16134eb695ece78194d
AkshayAglave007 <111517156+AkshayAglave007@users.noreply.github.com>
2022-08-27 23:12:59 +0530

    changed database

--- sms/sms/settings.py
+++ sms/sms/settings.py
@@ -75,13 +75,20 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sms_project',
+#         'USER':'postgres',
+#         'PASSWORD': 'psql@1339',
+#         'HOST': 'localhost',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sms_project',
-        'USER':'postgres',
-        'PASSWORD': 'psql@1339',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
